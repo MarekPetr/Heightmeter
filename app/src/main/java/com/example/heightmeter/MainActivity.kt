a3f73eb271901dc5c30a7357a4d17922d519cca5
Petr Marek <petr.marek18@gmail.com>
2024-12-27 16:44:26 +0100

    WIP: sensors data

--- app/src/main/java/com/example/heightmeter/MainActivity.kt
+++ app/src/main/java/com/example/heightmeter/MainActivity.kt
@@ -1,46 +1,54 @@
 package com.example.heightmeter
+
+import android.Manifest
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-
-import androidx.activity.ComponentActivity
-import androidx.activity.result.contract.ActivityResultContracts
-import android.Manifest
-import android.content.pm.PackageManager
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.LinearLayout
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.core.content.ContextCompat
-import com.google.common.util.concurrent.ListenableFuture
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.LifecycleOwner
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import kotlin.math.acos
+import kotlin.math.sqrt
+import kotlin.math.tan
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : ComponentActivity(), SensorEventListener {
+    private var sensorManager: SensorManager? = null
+    private var accelerometer: Sensor? = null
+    private var magnetometer: Sensor? = null
+    private var gravity = FloatArray(0)
+    private var geomagnetic = FloatArray(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setSensors();
+        initListeners();
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         setContent {
@@ -76,6 +84,87 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun setSensors() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magnetometer = sensorManager?.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+    }
+
+    private fun initListeners() {
+        sensorManager?.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager?.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            gravity = event.values
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            geomagnetic = event.values
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun computedAngle(): Double {
+        if (gravity.isEmpty() || geomagnetic.isEmpty()) {
+            throw Error("gravity or geomagnetic not initialized")
+        }
+
+        val R = FloatArray(9)
+        val I = FloatArray(9)
+
+        val gotRotationMatrix = SensorManager.getRotationMatrix(R, I, gravity, geomagnetic)
+        if (!gotRotationMatrix) {
+            throw Error("No rotation Matrix")
+        }
+
+        val orientation = FloatArray(3)
+        SensorManager.getOrientation(R, orientation)
+
+        val inclineGravity = gravity.clone()
+        val normOfG = sqrt(
+            inclineGravity[0] * inclineGravity[0] + inclineGravity[1] * inclineGravity[1] + inclineGravity[2] * inclineGravity[2]
+        ) as Float
+
+        // Normalize the accelerometer vector
+        inclineGravity[2] = (inclineGravity[2] / normOfG)
+
+        val arcCos = acos(inclineGravity[2]).toDouble()
+        val angle = Math.toDegrees(arcCos) - 90.0f
+        return Math.toRadians(angle)
+    }
+
+    private fun computeHeight(angle: Double, lensHeight: Double, distance: Double): Double {
+        val height = tan(angle * distance) + lensHeight
+        return Math.round(height * 10.0) * 0.1
+    }
+
+    private fun getHeightString(height: Double): String {
+        var minFlag = false
+        var maxFlag = false
+        if (height > 999999.9f) {
+            maxFlag = true
+        } else if (height < -99999.9f) {
+            minFlag = true
+        }
+
+        if (minFlag) {
+            return "min"
+        } else if (maxFlag) {
+            return "max"
+        } else {
+            val displayHeight = height.toString()
+            return displayHeight
+        }
+    }
+
+    private fun computeDistance(lensHeight: Double, distance: Double): Double {
+        val newDistance = lensHeight / (-(tan(computedAngle())))
+        return Math.round(newDistance * 10.0) * 0.1
     }
 }
 
@@ -113,26 +202,4 @@ fun bindPreview(cameraProvider: ProcessCameraProvider,
     cameraProvider.unbindAll() // Unbind use cases before rebinding
     val camera = cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview)
     preview.setSurfaceProvider(previewView.surfaceProvider)
-}
-
-class Sensors: SensorEventListener {
-    private var gravity = FloatArray(0)
-    private var geomagnetic = FloatArray(0)
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-            gravity = event.values
-        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
-            geomagnetic = event.values
-            getHeight()
-        }
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("Not yet implemented")
-    }
-
-    fun getHeight() {
-    }
-
 }
